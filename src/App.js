852575ceb66afed87a9c35e50e9d21c475dff872
corinsd <anduf424@gmail.com>
2022-05-17 18:15:45 +0900

    https://github.com/corinsd/tailwind-project.git

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello tailwind ~.
         </p>
         <a
           className="App-link"
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          tailwind reactproject start
         </a>
+        <h1 className="text-purple-500 font-mono ">hoeees</h1>
       </header>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const { scryRenderedComponentsWithType } = require("react-dom/test-utils");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
